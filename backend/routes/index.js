24f80693c2731cc577db1847f1963c7a1619bb75
Paulo Ricardo Dantas <rik.dantas@hotmail.com>
2022-10-04 23:08:15 -0300

    Questões agora ficam salvas no backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var questions = [];
+let inc = 1;
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('Login');
 });
 router.get('/TelaCadastro', function(req, res, next) {
   res.render('TelaCadastro');
+});
+
+router.post('/question', (req ,res) => {
+    let question = req.body;
+    question.id = inc;
+    questions.push(question);
+    console.log(questions);
+    res.send({question})
+    inc++;
+    return;
+});
+
+router.get('/question', (req ,res) => {
+    res.send({questions});
+    return;
+});
+
+router.delete('/question', (req ,res) => {
+  const index = req.params.id
+  console.log("apagou com sucesso");
+  return;
 });
 
 module.exports = router;
