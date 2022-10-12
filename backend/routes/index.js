8ae403f3b9129b13b94fcffbf2757c5476d79ef8
Paulo Ricardo Dantas <rik.dantas@hotmail.com>
2022-10-11 22:13:55 -0300

    Começando a implementação do login

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,30 +2,64 @@ var express = require('express');
 var router = express.Router();
 var questions = [];
 let inc = 1;
+const jwt = require('jsonwebtoken');
+const SECRET = "projetopes";
 
-router.get('/', function(req, res, next) {
+let users = [{
+  userid: "1",
+  username: "Professor",
+  password: "professor",
+  role: "teacher"
+},
+{
+  userid: "2",
+  username: "Aluno",
+  password: "aluno",
+  role: "student"
+}];
+
+router.get('/', function (req, res, next) {
   res.render('Login');
 });
-router.get('/TelaCadastro', function(req, res, next) {
+
+router.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return res.sendStatus(404);
+  }
+  if (password !== user.password) {
+    return res.sendStatus(404)
+  }
+
+  const token = jwt.sign({ userid: user.userid, role: user.role }, SECRET, { expiresIn: '1d' });
+
+  return res.json({ token: token, role: user.role });
+});
+
+router.get('/TelaCadastro', function (req, res, next) {
   res.render('TelaCadastro');
 });
 
-router.post('/question', (req ,res) => {
-    let question = req.body;
-    question.id = inc;
-    questions.push(question);
-    console.log(questions);
-    res.send({question})
-    inc++;
-    return;
+router.post('/question', (req, res) => {
+  console.log(req.headers);
+  let question = req.body;
+  question.id = inc;
+  questions.push(question);
+  console.log(questions);
+  res.send({ question })
+  inc++;
+  return;
 });
 
-router.get('/question', (req ,res) => {
-    res.send({questions});
-    return;
+router.get('/question', (req, res) => {
+  res.send({ questions });
+  return;
 });
 
-router.delete('/question', (req ,res) => {
+router.delete('/question', (req, res) => {
   const index = req.params.id
   console.log("apagou com sucesso");
   return;
